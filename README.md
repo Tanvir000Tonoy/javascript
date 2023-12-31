9f40dd9f35052a5ee20d9ef527b83d087e9b5940
Tanvir Modhu <happy2024.tat@gmail.com>
2024-01-01 02:37:48 +0600

    array and object
    recap v01

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # javascript
 This repository is for my personal use only. I will continue my js practice here and store all the garbage here 😊
+### DATE 1st January 2024
+hello world :) 
---
+++ main.js
@@ -0,0 +1,15 @@
+let array = [
+    {
+        name: "Tanvir Tonoy",
+        age : 20,
+        class : 13,
+        wealth : 2800
+    },
+    {
+        name: "Tamim",
+        age: 21,
+        class: 13,
+        wealth: null
+    }
+]
+console.log(array[1].name);
