269cd7fe87b0bcf5ef0775b09dd6daef32bdb2b9
Tanvir Modhu <happy2024.tat@gmail.com>
2024-01-01 14:08:53 +0600

    function recap
    function is kinda exciting to make

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # javascript
+
 This repository is for my personal use only. I will continue my js practice here and store all the garbage here 😊
+
 ### DATE 1st January 2024
-hello world :) 
+
+hello world :)
--- main.js
+++ main.js
@@ -1,15 +1,5 @@
-let array = [
-    {
-        name: "Tanvir Tonoy",
-        age : 20,
-        class : 13,
-        wealth : 2800
-    },
-    {
-        name: "Tamim",
-        age: 21,
-        class: 13,
-        wealth: null
-    }
-]
-console.log(array[1].name);
+function myfuntion(a, b, c) {
+  return a + b + c;
+}
+
+console.log(myfuntion(10, 20, 30));
