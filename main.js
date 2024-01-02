8f631bae2d37272e1a8b54459b534c957879756d
Tanvir Modhu <happy2024.tat@gmail.com>
2024-01-02 07:06:45 +0600

    update v2
    my devies and some array methods

--- main.js
+++ main.js
@@ -10,29 +10,30 @@
 // variable --Done
 // if else , loop , --Done
 // Methods (string, array, more) --in Progress
+*/
 
 let fruits = ["apple", "Banana", "Orange"];
 console.log(fruits);
 let myDevices = [
-    "Nokia lumia 311",
-    "Lenovo tab 3",
-    "Nokia lumia 730",
-    "symphony i10",
-    "walton primo",
-    "Lenovo thinkpad t420s",
-    "oppoa92020",
-    {
-        name: "custom PC",
-        cost: 99000,
-        cpu: "AMD Ryzed 3700x",
-        ram: "16gb",
-        ssd: 500, // in GB
-        "mother-board": "Rog strix b550-f gaming wifi addition",
-        gpu: "gt 1030oc", // later upgradded to 3060s
-        monitor: "hp 22fw",
-    },
-    "macbook air m1 2020",
-    "iphone 14 pro max",
+  "Nokia lumia 311",
+  "Lenovo tab 3",
+  "Nokia lumia 730",
+  "symphony i10",
+  "walton primo",
+  "Lenovo thinkpad t420s",
+  "oppoa92020",
+  {
+    name: "custom PC",
+    cost: 99000,
+    cpu: "AMD Ryzed 3700x",
+    ram: "16gb",
+    ssd: 500, // in GB
+    "mother-board": "Rog strix b550-f gaming wifi addition",
+    gpu: "gt 1030oc", // later upgradded to 3060s
+    monitor: "hp 22fw",
+  },
+  "macbook air m1 2020",
+  "iphone 14 pro max",
 ];
 console.log(myDevices);
 // concatinate two different array into one array
@@ -40,17 +41,15 @@ let newArray = [...fruits, ...myDevices];
 console.log(newArray);
 //  array destructuring
 for (i = 0; i <= newArray.length - 1; i++) {
-    //   console.log(typeof newArray[10]);
-    if (typeof newArray[i] == "object") {
-        console.log(newArray[i].name);
-    } else if (typeof newArray[i] == "string") {
-        console.log(newArray[i]);
-    }
+  //   console.log(typeof newArray[10]);
+  if (typeof newArray[i] == "object") {
+    console.log(newArray[i].name);
+  } else if (typeof newArray[i] == "string") {
+    console.log(newArray[i]);
+  }
 }
 
 //  i also now the push and pop methods , also shif , unshift methods lets try those things also
-
-*/
 
 let array = ["item1", "item2", "item3"];
 console.log(array);
