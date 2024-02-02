df4cec10fc8a1a08789a808a4e137d690196aeeb
Tanvir Modhu <happy2024.tat@gmail.com>
2024-02-02 06:45:58 +0600

    functions
    part 1

---
+++ tanvir.js
@@ -0,0 +1,16 @@
+// different type of functions 
+function typeOne(){
+console.log(100);
+}
+
+let typeTwo = function(){
+console.log(100);
+}
+
+let typeThree = ()=>{
+console.log(100);
+}
+
+typeOne();
+typeTwo();
+typeThree();
